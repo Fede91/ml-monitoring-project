d10f7749ac71baab60183a5735f48c6ac3fd06d6
Fedev <Fede91@users.noreply.github.com>
2025-06-25 23:32:19 +0200

    feat: implement app tests

---
+++ tests/app/test_model.py
@@ -0,0 +1,38 @@
+from app.model import predict
+from app.log import log
+
+def test_positive_predict():
+
+  text = "I love this project!!!"
+  target = "positive"
+
+  label, scores = predict(text, target)
+
+  assert label == "positive"
+  assert scores[0] < 0.5
+  assert scores[1] < 0.5
+  assert scores[2] > 0.5
+
+def test_negative_predict():
+
+  text = "I hate this project!!!"
+  target = "negative"
+
+  label, scores = predict(text, target)
+
+  assert label == "negative"
+  assert scores[0] > 0.5
+  assert scores[1] < 0.5
+  assert scores[2] < 0.5
+
+def test_neutral_predict():
+
+  text = "I'm working on this project"
+  target = "neutral"
+
+  label, scores = predict(text, target)
+
+  assert label == "neutral"
+  assert scores[0] < 0.5
+  assert scores[1] > 0.5
+  assert scores[2] < 0.5
