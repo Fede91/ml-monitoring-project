aa0d9474ba343094b61510a1f4e3cd90042a5e86
Fedev <Fede91@users.noreply.github.com>
2025-06-26 08:53:57 +0200

    feat: implement export logs to HF Dataset

--- app/log.py
+++ app/log.py
@@ -2,8 +2,15 @@
 import pandas as pd
 import os
 from datetime import datetime
+from datasets import Dataset
 
 LOG_PATH = "logs/inference_log.csv"
+
+def export_logs():
+  df = pd.read_csv(LOG_PATH)
+
+  ds = Dataset.from_pandas(df)
+  ds.push_to_hub("0xfedev/corporate-sentiment-logs", token=os.getenv('HF_TOKEN'))
 
 def log(texts, targets, prediction, scores):
   df = pd.DataFrame({
@@ -13,8 +20,13 @@ def log(texts, targets, prediction, scores):
       "scores": scores,
       "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
   })
+  
   os.makedirs(os.path.dirname(LOG_PATH), exist_ok=True)
+
   if os.path.exists(LOG_PATH):
-      df.to_csv(LOG_PATH, mode="a", header=False, index=False)
+    df.to_csv(LOG_PATH, mode="a", header=False, index=False)
   else:
-      df.to_csv(LOG_PATH, index=False)
+    df.to_csv(LOG_PATH, index=False)
+  
+  export_logs()
+  
